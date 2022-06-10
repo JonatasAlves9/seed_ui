3b6b03cdd57b96849ddff973d5fff527b87f7b10
JonatasAlves9 <alvesjonatas99@gmail.com>
2022-06-10 17:05:16 -0300

    feat: create component of alert

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,29 +1,18 @@
 import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply, TesteView } from 'react-native-seedui';
+import { Alert } from 'react-native-seedui';
+import { View } from 'react-native';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
   return (
-    <TesteView />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'red',
+      }}
+    >
+      <Alert />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});

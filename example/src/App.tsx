0693bdd2ff35a74e4d961a46504017fb6a631cb6
JonatasAlves9 <alvesjonatas99@gmail.com>
2022-06-09 22:10:25 -0300

    feat: create view test

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-seedui';
+import { multiply, TesteView } from 'react-native-seedui';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
@@ -11,9 +11,7 @@ export default function App() {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <TesteView />
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,26 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
+
+export const TesteView = () => {
+  const [result, setResult] = React.useState<number | undefined>();
+
+  React.useEffect(() => {
+    multiply(3, 7).then(setResult);
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: 'red',
+      }}
+    >
+      <Text>Result: {result}</Text>
+    </View>
+  );
+};
